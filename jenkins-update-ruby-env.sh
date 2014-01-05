4aac0de2a092ac6d0c0262c1921e25d483a15c68
tcox5698 <tcox56_98@yahoo.com>
2014-01-05 09:46:18 -0600

    next step - clone jobs into jenkins

--- jenkins-update-ruby-env.sh
+++ jenkins-update-ruby-env.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-gem update bundler
-rvm cleanup all
-rvm pkg uninstall libxml2
-rvm reinstall all --force
+echo UPDATING RUBY ENVIRONMENT
+
+set -e
+
+if [ -e ruby-updated ]
+  then
+    gem update bundler
+    rvm cleanup all
+    rvm pkg uninstall libxml2
+
+    echo RVM REINSTALL ALL FORCE
+    rvm reinstall 2.1.0 --force
+    touch ruby-updated
+    echo RVM REINSTALL ALL FORCE COMPLETE
+  else
+    echo RUBY ALREADY UPDATED according to ruby-updated file
+fi
+echo UPDATING RUBY ENVIRONMENT COMPLETE
