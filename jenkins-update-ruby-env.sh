535ff3f1eb33b69b84ae012aa47259482e9d9722
tcox5698 <tcox56_98@yahoo.com>
2014-01-05 16:51:00 -0600

    now cloning railsplay project

--- jenkins-update-ruby-env.sh
+++ jenkins-update-ruby-env.sh
@@ -6,6 +6,8 @@ set -e
 
 if [ -e ruby-updated ]
   then
+    echo RUBY ALREADY UPDATED according to ruby-updated file
+  else
     gem update bundler
     rvm cleanup all
     rvm pkg uninstall libxml2
@@ -14,7 +16,5 @@ if [ -e ruby-updated ]
     rvm reinstall 2.1.0 --force
     touch ruby-updated
     echo RVM REINSTALL ALL FORCE COMPLETE
-  else
-    echo RUBY ALREADY UPDATED according to ruby-updated file
 fi
 echo UPDATING RUBY ENVIRONMENT COMPLETE
