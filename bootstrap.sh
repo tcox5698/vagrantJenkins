2c7b77bacbf349320a64a18bf233a3b91abce128
tcox5698 <tcox56_98@yahoo.com>
2014-01-05 05:09:43 -0600

    whew jenkins is installing with rvm and rails now

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,45 @@
 SPATH=`pwd`
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo echo 'deb http://pkg.jenkins-ci.org/debian binary/' >> /etc/apt/sources.list
+set -e
 
+echo CONFIGURING APT-JENKINS
+if [ -e apt-jenkins-configured ]
+  then echo apt jenkins configured
+  else
+    wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+    sudo echo 'deb http://pkg.jenkins-ci.org/debian binary/' >> /etc/apt/sources.list
+    touch apt-jenkins-configured
+fi
+echo CONFIGURING APT-JENKINS COMPLETE
+
+echo APT-GET INSTALLS
 sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y jenkins
 sudo apt-get install -y postgresql
 sudo apt-get install -y curl
 
+sudo usermod -a -G admin jenkins
 
-
+echo MAKE WORK DIR
 if [ -e ./work ]
   then echo work dir exists
   else mkdir work
 fi
+echo MAKE WORK DIR COMPLETE
 
+echo DOWNLOAD JENKINS CLIENT
 if [ -e ./work/jenkins-cli.jar ]
   then echo jenkins-cli exists
   else
     cd work
+    sleep 10
     wget http://localhost:8080/jnlpJars/jenkins-cli.jar
     chmod a=rwx jenkins-cli.jar
     cd $SPATH
 fi
+echo DOWNLOAD JENKINS CLIENT COMPLETE
 
-su jenkins /vagrant/jenkins-setup-rails.sh
+echo JENKINS-SETUP
+su -l jenkins /vagrant/jenkins-setup-rails.sh
+echo JENKINS-SETUP COMPLETE
--- jenkins-setup-rails.sh
+++ jenkins-setup-rails.sh
@@ -2,13 +2,38 @@
 
 #SETS UP JENKINS USER WITH RVM AND RAILS - RUN AS JENKINS USER
 
-set -e
+echo WMO AM I
+echo `whoami`
 
-\curl -sSL https://get.rvm.io | bash -s stable --rails
+echo CURRENT DIR
+echo `pwd`
 
-echo "[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm" > /home/vagrant/.profile
+#set -e
 
-echo "---\ngem: --no-ri --no-rdoc" > /home/vagrant/.gemrc
+echo INSTALL RVM AND RAILS
+curl -sSL https://get.rvm.io | bash -s stable --rails
+echo INSTALL RVM AND RAILS COMPLETE
+
+echo CONFIG PROFILE WITH RVM
+if [ -e profile-rvm-configged ]
+  then echo profile configged with rvm already
+  else
+    echo TRYING TO WRITE TO /home/jenkins/.profile
+    echo "[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm" > /home/jenkins/.profile
+    echo FINISHED WRITING TO .profile
+    touch profile-rvm-configged
+fi
+echo CONFIG PROFILE WITH RVM COMPLETE
+
+echo CONGIF GEMRC
+if [ -e gemrc-rvm-configged ]
+  then echo gemrc configged with rvm already
+  else
+    echo "---\ngem: --no-ri --no-rdoc" > /home/jenkins/.gemrc
+    touch gemrc-rvm-configged
+fi
+
+echo CONFIG GEMRC COMPLETE
 
 git config --global user.email "jenkins@tcdev.com"
 git config --global user.name "Jenkins TCDEV"
